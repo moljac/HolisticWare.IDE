005dfc2ef9e7f3112ddc839ce4e7a32225b34b2f
moljac <mcvjetko@holisticware.net>
2014-11-29 23:51:43 +0100

    filesearch for scraping initial commit

--- AddIns-Extensions/HolisticWare.IDE.Tools.Projects.ProjectTypeGuidScraper.EXE.Cons/Program.cs
+++ AddIns-Extensions/HolisticWare.IDE.Tools.Projects.ProjectTypeGuidScraper.EXE.Cons/Program.cs
@@ -18,6 +18,14 @@ namespace HolisticWare.IDE.Tools.Projects.ProjectTypeGuidScraper.EXE
 			//	*.cxproj
 			//	?????
 			Project project = new Project();
+
+
+			string[] files = null;
+			string searchPattern = "*proj";
+
+			string path = System.IO.Path.Combine("..", "..", "..", "..", "Templates", "Projects");
+			files = System.IO.Directory.GetFiles(path, searchPattern, System.IO.SearchOption.AllDirectories);
+
 			return;
 		}
 	}
