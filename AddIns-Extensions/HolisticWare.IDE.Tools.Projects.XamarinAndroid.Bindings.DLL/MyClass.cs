b3c811cb0dc0247c64a9ce83c82407fdfa31e854
moljac <mcvjetko@holisticware.net>
2015-05-17 23:45:11 +0200

    Xamarin Android api.xml parsing

---
+++ AddIns-Extensions/HolisticWare.IDE.Tools.Projects.XamarinAndroid.Bindings.DLL/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolisticWare.IDE.Tools.Projects.XamarinAndroid.Bindings.DLL
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ AddIns-Extensions/HolisticWare.IDE.Tools.Projects.XamarinAndroid.Bindings.EXE.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolisticWare.IDE.Tools.Projects.XamarinAndroid.Bindings.EXE.Console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
